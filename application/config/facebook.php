b58cea6e4f686dd0118c6d6638eac9e04e9db360
Tim Mahoney <tim@createreach.com>
2011-12-29 17:16:30 -0500

    Updated Facebook library and added a facebook config file so that the facebook cookie gets taken care of correctly.

---
+++ application/config/facebook.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config["appId"] = "APP ID";
+$config["secret"] = "APP SECRET";
+$config["fileUpload"] = false;
+
+
+/* End of file facebook.php */
+/* Location: ./application/config/facebook.php */
